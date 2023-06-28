b74b90a1d34b0bd79d7ae5ed44b1b25729805579
Daniel <65313279+dtnech@users.noreply.github.com>
2023-06-27 23:42:54 -0500

    Update main.py

--- notebooks/data_acquisition/main.py
+++ notebooks/data_acquisition/main.py
@@ -1,1 +1,14 @@
-
+## Adquisición de Datos
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+import tensorflow as tf
+%load_ext tensorboard
+import tensorboard
+tensorboard.__version__
+import matplotlib.pyplot as plt
+from datetime import datetime
+df = pd.read_csv('/content/creditcard.csv', sep=';')
+df.head()
