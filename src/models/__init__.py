e85dc8ba28496a534278d5cb12a8b543da55a901
Daniel <65313279+dtnech@users.noreply.github.com>
2023-07-05 00:08:53 -0500

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,8 @@
+early_stopp = EarlyStopping(patience = 20, min_delta = 0.001, 
+                                               restore_best_weights =True )
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 
+escalado = StandardScaler()
+
+x_train_escalado = escalado.fit_transform(x_train)
+x_test_escalado = escalado.transform(x_test)
